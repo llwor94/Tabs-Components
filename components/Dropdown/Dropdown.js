50db3aa769ddcd7f4c06363966c9350514158743
Daniel Frehner <dfrehner1@gmail.com>
2018-05-18 17:18:12 -0600

    solution code

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,17 +1,16 @@
 class Dropdown {
-  constructor($element) {
-    this.element 
-    // Select button and content from the element
-    this.button
-    this.content 
-    // Add a click handler to the button
+  constructor(element) {
+    this.element = element; 
+    this.button = this.element.querySelector('.dropdown-button');
+    this.content = this.element.querySelector('.dropdown-content');
+    this.button.addEventListener('click', () => { this.toggleContent() });
 
   }
 
   toggleContent() {
-    // Add/remove class to/from content
+    this.content.classList.toggle('dropdown-hidden');
   }
 }
 
-let $dropdown = $('.dropdown');
-$dropdown = new Dropdown($dropdown);
+let dropdowns = document.querySelectorAll('.dropdown');
+dropdowns = Array.from(dropdowns).map( dropdown => new Dropdown(dropdown));
